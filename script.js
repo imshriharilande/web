d888f7672b5ab1584ef2466a0616485cc5e0a881
Shrihari lande <130351861+imshriharilande@users.noreply.github.com>
2024-05-24 16:14:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Traveller", "Developer", "Data Analyst"],
+        strings: ["Web Developer", "Developer", "Data Analyst"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Traveller", "Developer", "Data Analyst"],
+        strings: ["Web Developer", "Developer", "Data Analyst"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
